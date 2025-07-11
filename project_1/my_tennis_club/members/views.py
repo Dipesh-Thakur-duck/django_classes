cdcbc08ce8534e0f7f3519009f21ad7016677b1b
Dipesh Thakur <letsmaildipesh1@gmail.com>
2025-07-11 13:32:46 +0545

    Add code to integrate db
    Database integration

--- project_1/my_tennis_club/members/views.py
+++ project_1/my_tennis_club/members/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 def members(request):
-    return HttpResponse("Hello World!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
